5cfdb0f7315d8e57f9c35374123f8a4a1c4e2957
Kamil Nowak <kamilnowakx98@gmail.com>
2019-09-21 22:39:56 +0200

    Updates

---
+++ precache-manifest.58b764f8202d3f0a258729bcd4e0c9ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8117368198bc5461d2821a3339bffe",
+    "url": "/The-Game-Of-Live/index.html"
+  },
+  {
+    "revision": "aeeb36667192aa0b4574",
+    "url": "/The-Game-Of-Live/static/css/main.ac98e15d.chunk.css"
+  },
+  {
+    "revision": "d64df08882360fd1bc05",
+    "url": "/The-Game-Of-Live/static/js/2.f3e95dd5.chunk.js"
+  },
+  {
+    "revision": "aeeb36667192aa0b4574",
+    "url": "/The-Game-Of-Live/static/js/main.b523000e.chunk.js"
+  },
+  {
+    "revision": "b5a3a13ddd969d8ca290",
+    "url": "/The-Game-Of-Live/static/js/runtime~main.2440da77.js"
+  }
+]);
